48703fc01357c5c123c963619e519fa704610f52
maxim <maximilian.danielsson@gmail.com>
2023-12-19 08:49:13 +0100

    Add test_grids.jl

---
+++ test/test_grids.jl
@@ -0,0 +1,18 @@
+using Revise
+
+using MolecularEvolution, CodonMolecularEvolution
+
+treestr, tags, tag_colors = import_colored_figtree_nexus_as_tagged_tree(".\\Trees\\Ace2_tiny_tagged.nex")
+seqnames, seqs = read_fasta(".\\Trees\\Ace2_tiny_tagged.fasta")
+analysis_name = ".\\Paralllel\\Results\\"
+
+tree, tags, tag_colors, analysis_name = CodonMolecularEvolution.difFUBAR_init(analysis_name, treestr, tags, tag_colors, exports=false, verbosity=1)
+code = MolecularEvolution.universal_code
+tree, my_alpha, beta, GTRmat, F3x4_freqs, eq_freqs = CodonMolecularEvolution.difFUBAR_global_fit(seqnames, seqs, tree, CodonMolecularEvolution.generate_tag_stripper(tags), code, verbosity=1) #60s
+
+tree_no_prune = deepcopy(tree)
+tree_prune_1 = deepcopy(tree)
+tree_prune_2 = deepcopy(tree)
+
+time_difFUBAR = @elapsed con_lik_matrix_no_prune, log_con_lik_matrix, codon_param_vec, alphagrid, omegagrid, param_kinds = CodonMolecularEvolution.difFUBAR_grid(tree_no_prune, tags, GTRmat, F3x4_freqs, code, verbosity=0, foreground_grid=6, background_grid=4) # 50s
+time_difFUBAR_prune_max = @elapsed con_lik_matrix_prune_1, log_con_lik_matrix, codon_param_vec, alphagrid, omegagrid, param_kinds = CodonMolecularEvolution.difFUBAR_grid_pruned_1(tree_prune_1, tags, GTRmat, F3x4_freqs, code, verbosity=0, foreground_grid=6, background_grid=4) # 28s
