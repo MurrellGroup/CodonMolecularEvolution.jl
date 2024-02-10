9a3227686389fc96f6f4216ac3dad7d58cdbf8e4
Maximilian Danielsson <maximilian.danielsson@gmail.com>
2024-02-10 01:49:00 +0100

    Include grids.jl

--- src/CodonMolecularEvolution.jl
+++ src/CodonMolecularEvolution.jl
@@ -1,9 +1,10 @@
 module CodonMolecularEvolution
 
-using FASTX, MolecularEvolution, Measures, Compose, PyPlot, StatsBase, Distributions, DataFrames, CSV, NLopt, ParameterHandling
+using FASTX, MolecularEvolution, Measures, Compose, PyPlot, StatsBase, Distributions, DataFrames, CSV, NLopt, ParameterHandling#, LinearAlgebra
 
 include("shared/shared.jl")
 include("difFUBAR/difFUBAR.jl")
+include("difFUBAR/grids.jl")
 
 # Write your package code here.
 
