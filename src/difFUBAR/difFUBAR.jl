4adddc4b20fbeeae3345ca3d60ffb5b3259294cb
Maximilian Danielsson <maximilian.danielsson@gmail.com>
2024-06-05 15:19:51 +0200

    Update the difFUBAR UI

--- src/difFUBAR/difFUBAR.jl
+++ src/difFUBAR/difFUBAR.jl
@@ -158,82 +158,21 @@ function difFUBAR_global_fit_2steps(seqnames, seqs, tree, leaf_name_transform, c
 end
 
 #foreground_grid and background_grid control the number of categories below 1.0
-function difFUBAR_grid(tree, tags, GTRmat, F3x4_freqs, code; verbosity = 1, foreground_grid = 6, background_grid = 4)
+function difFUBAR_grid(tree, tags, GTRmat, F3x4_freqs, code; verbosity = 1, foreground_grid = 6, background_grid = 4, t = 0)
 
-    cached_model = MG94_cacher(code)
-
-    #This is the function that assigns models to branches
-    #Sometimes there will be the same number of tags as omegas, but sometimes there will be one more omega.
-    #Depending on whether there are any background branches (UNTESTED WITH NO BACKGROUND BRANCHES)
-    function N_Omegas_model_func(tags,omega_vec,alpha,nuc_mat,F3x4, code)
-        models = [cached_model(alpha,alpha*o,nuc_mat,F3x4, genetic_code = code) for o in omega_vec];
-        return n::FelNode -> [models[model_ind(n.name,tags)]]
-    end
-
-    #Defines the grid used for inference.
-    function gridsetup(lb, ub, num_below_one, trin, tr)
-        step = (trin(1.0) - trin(lb))/num_below_one
-        return tr.(trin(lb):step:trin(ub))
-    end
-    tr(x) = 10^x-0.05
-    trinv(x) =  log10(x+0.05)
-    alphagrid = gridsetup(0.01, 13.0, foreground_grid, trinv, tr); 
-    omegagrid = gridsetup(0.01, 13.0, foreground_grid, trinv, tr)
-    background_omega_grid = gridsetup(0.05, 6.0, background_grid, trinv, tr) #Much coarser, because this isn't a target of inference
-    length(background_omega_grid) * length(alphagrid) * length(omegagrid)^2
-
-    num_groups = length(tags)
-    is_background = maximum([model_ind(n.name, tags) for n in getnodelist(tree) if !isroot(n)]) > num_groups
-    tensor_dims = 1+num_groups+is_background;
-
-    function add_to_each_element(vec_of_vec, elems)
-        return [vcat(v,[e]) for v in vec_of_vec for e in elems]
-    end
-    
-    codon_param_vec = [[a] for a in alphagrid]
-    param_kinds = ["Alpha"]
-    for g in 1:num_groups
-        push!(param_kinds, "OmegaG$(g)")
-        codon_param_vec = add_to_each_element(codon_param_vec,omegagrid)
-    end
-    if is_background
-        push!(param_kinds, "OmegaBackground")
-        codon_param_vec = add_to_each_element(codon_param_vec,background_omega_grid)
-    end
-    codon_param_vec;
-    
-    num_sites = tree.message[1].sites
-    l = length(codon_param_vec)
-    log_con_lik_matrix = zeros(l,num_sites);
-
-    verbosity > 0 && println("Step 3: Calculating grid of $(length(codon_param_vec))-by-$(tree.message[1].sites) conditional likelihood values (the slowest step). Currently on:")
-
-    for (row_ind,cp) in enumerate(codon_param_vec)
-        alpha = cp[1]
-        omegas = cp[2:end]
-        tagged_models = N_Omegas_model_func(tags,omegas,alpha,GTRmat,F3x4_freqs, code)
-
-        felsenstein!(tree,tagged_models)
-        #This combine!() is needed because the current site_LLs function applies to a partition
-        #And after a felsenstein pass, you don't have the eq freqs factored in.
-        #We could make a version of log_likelihood() that returns the partitions instead of just the sum
-        combine!.(tree.message,tree.parent_message)
-
-        log_con_lik_matrix[row_ind,:] .= MolecularEvolution.site_LLs(tree.message[1]) #Check that these grab the scaling constants as well!
-        verbosity > 0 && if mod(row_ind,500)==1
-            print(round(100*row_ind/length(codon_param_vec)),"% ")
-            flush(stdout)
-        end
-    end
-    verbosity > 0 && println()
-
-    con_lik_matrix = zeros(size(log_con_lik_matrix));
-    site_scalers = maximum(log_con_lik_matrix, dims = 1);
-    for i in 1:num_sites
-        con_lik_matrix[:,i] .= exp.(log_con_lik_matrix[:,i] .- site_scalers[i])
-    end
-
-    return con_lik_matrix, log_con_lik_matrix, codon_param_vec, alphagrid, omegagrid, param_kinds
+    log_con_lik_matrix, codon_param_vec, alphagrid, omegagrid, background_omega_grid, param_kinds, is_background, num_groups, num_sites = gridprep(tree, tags; 
+                                                                                                                                                    verbosity = verbosity, 
+                                                                                                                                                    foreground_grid = foreground_grid, 
+                                                                                                                                                    background_grid = background_grid
+                                                                                                                                                    )
+    heuristic_pick, nthreads = choose_grid_and_nthreads(tree, tags, num_groups, num_sites, alphagrid, omegagrid, background_omega_grid, code)
+    nthreads = t > 0 ? t : nthreads
+    verbosity > 1 && println("\n$(heuristic_pick), with $(nthreads) parallel threads will be used for the grid likelihood calculations\n") #Higher level of verbosity
+    return heuristic_pick(tree, tags, GTRmat, F3x4_freqs, code, log_con_lik_matrix, codon_param_vec, alphagrid, omegagrid, background_omega_grid, param_kinds, is_background, num_groups, num_sites, nthreads; 
+                        verbosity = verbosity, 
+                        foreground_grid = foreground_grid,
+                        background_grid = background_grid
+                        )
 end
 
 function difFUBAR_sample(con_lik_matrix, iters; verbosity = 1)
@@ -421,12 +360,12 @@ end
 export difFUBAR_tabulate
 
 #Must return enough to re-calculate detections etc
-function difFUBAR(seqnames, seqs, treestring, tags, tag_colors, outpath; pos_thresh = 0.95, iters = 2500, verbosity = 1, exports = true, code = MolecularEvolution.universal_code)
+function difFUBAR(seqnames, seqs, treestring, tags, tag_colors, outpath; pos_thresh = 0.95, iters = 2500, verbosity = 1, exports = true, code = MolecularEvolution.universal_code, optimize_branch_lengths = false, t = 0)
     analysis_name = outpath
     tree, tags, tag_colors, analysis_name = difFUBAR_init(analysis_name, treestring, tags, tag_colors, exports = exports, verbosity = verbosity)
-    tree, alpha,beta,GTRmat,F3x4_freqs,eq_freqs = difFUBAR_global_fit(seqnames, seqs, tree, generate_tag_stripper(tags), code, verbosity = verbosity)
+    tree, alpha,beta,GTRmat,F3x4_freqs,eq_freqs = difFUBAR_global_fit_2steps(seqnames, seqs, tree, generate_tag_stripper(tags), code, verbosity = verbosity, optimize_branch_lengths = optimize_branch_lengths)
     con_lik_matrix, _, codon_param_vec, alphagrid, omegagrid, _ = difFUBAR_grid(tree, tags, GTRmat, F3x4_freqs, code, 
-                                                                                verbosity = verbosity, foreground_grid = 6, background_grid = 4)
+                                                                                verbosity = verbosity, foreground_grid = 6, background_grid = 4, t = t)
     alloc_grid,theta = difFUBAR_sample(con_lik_matrix, iters, verbosity = verbosity)
     df = difFUBAR_tabulate(analysis_name, pos_thresh, alloc_grid, codon_param_vec, alphagrid, omegagrid, tag_colors; verbosity = verbosity, exports = exports)
 
