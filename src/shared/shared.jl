80199f037eb95e8676c78f871555eb826be5170c
patruong <ptruong@kth.se>
2024-06-13 08:55:30 +0000

    added more retagging functions

--- src/shared/shared.jl
+++ src/shared/shared.jl
@@ -156,7 +156,6 @@ function generate_hex_colors_predefined(num_colors)
     end
 end
 
-
 function get_node_by_name(tree, nodename)
     mynode = []
     for node in getnodelist(tree)
@@ -239,31 +238,6 @@ end
 export import_grouped_labelled_newick_tree
 
 
-
-function import_grouped_labelled_newick_tree_reroot(tree_file::String, node_name::String, dist_above_child::Float64, recursive_retagging::Bool=false)
-    # Takes a Newick tree file and return Newick tree, Newick tree with replaced tags, group tags, original tags, and randomly generated colours for each tag
-    tree = read_newick_tree(tree_file)
-    node = get_node_by_name(tree, node_name)
-    tree = MolecularEvolution.recursive_reroot(node, dist_above_child=dist_above_child)
-
-    if recursive_retagging == false
-        treestring = newick(tree)
-        treestring_group_labeled, group_tags, original_tags = replace_newick_tags(treestring)
-        tag_colors = generate_hex_colors_predefined(length(original_tags))
-    else
-        remove_internal_node_tags(tree)
-        recursive_tagging(tree)
-        treestring = newick(tree)
-        treestring_group_labeled, group_tags, original_tags = CodonMolecularEvolution.replace_newick_tags(treestring)
-        tag_colors = CodonMolecularEvolution.generate_hex_colors_predefined(length(original_tags))
-    end
-    return treestring_group_labeled, treestring, group_tags, original_tags, tag_colors
-end
-
-export import_grouped_labelled_newick_tree_reroot
-
-
-
 function select_analysis_tags_from_newick_tree(tags, tag_colors, tag_pos)
     # If there are more than 2 tags in the newick tree, we split up the tags to the tags to be analyzed and the tags to be removed (placed as background)
     analysis_tags = tags[tag_pos]
@@ -296,14 +270,17 @@ Optimizes the MG94+F3x4 model on a tree, given a set of sequences and a tree. Re
 The leaf_name_transform kwarg can be used to transform the leaf names in the tree to match the seqnames.
 """
 function optimize_MG94_F3x4(seqnames, seqs, tree; leaf_name_transform=x -> x, genetic_code=MolecularEvolution.universal_code)
+
     #Count F3x4 frequencies from the seqs, and estimate codon freqs from this
     f3x4 = MolecularEvolution.count_F3x4(seqs)
     eq_freqs = MolecularEvolution.F3x4_eq_freqs(f3x4)
 
     #Set up a codon partition (will default to Universal genetic code)
-    initial_partition = CodonPartition(Int64(length(seqs[1]) / 3), code=genetic_code)
-    initial_partition.state .= eq_freqs
+    eq_partition = CodonPartition(Int64(length(seqs[1]) / 3), code=genetic_code)
+    eq_partition.state .= eq_freqs
+    initial_partition = LazyPartition{CodonPartition}(nothing)
     populate_tree!(tree, initial_partition, seqnames, seqs, leaf_name_transform=leaf_name_transform)
+    lazyprep!(tree, [eq_partition])
 
     #We'll use the empirical F3x4 freqs, fixed MG94 alpha=1, and optimize the nuc parameters and MG94 beta
     #Note: the nuc rates are confounded with alpha
@@ -330,14 +307,136 @@ function optimize_MG94_F3x4(seqnames, seqs, tree; leaf_name_transform=x -> x, ge
     upper_bounds!(opt, [5.0 for i in 1:num_params])
     xtol_rel!(opt, 1e-12)
     _, mini, _ = NLopt.optimize(opt, flat_initial_params)
+
     final_params = unflatten(mini)
-    final_params.rates
+
     #tree, alpha, beta, nuc_matrix, F3x4, eq_freqs
     return tree, 1.0, final_params.beta, reversibleQ(final_params.rates, ones(4)), f3x4, eq_freqs
 end
 
+function optimize_nuc_mu(seqnames, seqs, tree; leaf_name_transform=x -> x, genetic_code=MolecularEvolution.universal_code, optimize_branch_lengths=false)
+    #Optimize mu params using a nuc model, mu denotes the nucleotide mutational biases
+    nuc_pi = char_proportions(seqs, MolecularEvolution.nucstring)
 
+    eq_partition = NucleotidePartition(length(seqs[1]))
+    eq_partition.state .= nuc_pi
+    if optimize_branch_lengths == true
+        populate_tree!(tree, eq_partition, seqnames, seqs, leaf_name_transform=leaf_name_transform)
+    else
+        initial_partition = LazyPartition{NucleotidePartition}(nothing)
+        populate_tree!(tree, initial_partition, seqnames, seqs, leaf_name_transform=leaf_name_transform)
+        lazyprep!(tree, [eq_partition])
+    end
 
+    initial_params = positive(ones(6)) #rates must be non-negative
+
+    flat_initial_params, unflatten = value_flatten(initial_params) #See ParameterHandling.jl docs
+    num_params = length(flat_initial_params)
+
+    function build_model_vec(p; nuc_pi=nuc_pi)
+        return GeneralCTMC(reversibleQ(p, nuc_pi))
+    end
+
+    function objective(params; tree=tree)
+        return -log_likelihood!(tree, build_model_vec(params))
+    end
+
+    opt = Opt(:LN_BOBYQA, num_params)
+    min_objective!(opt, (x, y) -> (objective ∘ unflatten)(x))
+    lower_bounds!(opt, [-5.0 for i in 1:num_params])
+    upper_bounds!(opt, [5.0 for i in 1:num_params])
+    xtol_rel!(opt, 1e-12)
+    _, mini, _ = NLopt.optimize(opt, flat_initial_params)
+
+    final_params = unflatten(mini)
+    #tree, nuc mu rates, nuc pi rates
+    return tree, final_params, nuc_pi
+end
+
+function optimize_codon_alpha_and_beta(seqnames, seqs, tree, GTRmat; leaf_name_transform=x -> x, genetic_code=MolecularEvolution.universal_code)
+    #Now we optimize alpha and beta rates using a codon model
+    #Count F3x4 frequencies from the seqs, and estimate codon freqs from this
+    f3x4 = MolecularEvolution.count_F3x4(seqs)
+    eq_freqs = MolecularEvolution.F3x4_eq_freqs(f3x4)
+
+    #Set up a codon partition (will default to Universal genetic code)
+    eq_partition = CodonPartition(Int64(length(seqs[1]) / 3), code=genetic_code)
+    eq_partition.state .= eq_freqs
+    initial_partition = LazyPartition{CodonPartition}(nothing)
+    populate_tree!(tree, initial_partition, seqnames, seqs, leaf_name_transform=leaf_name_transform)
+    lazyprep!(tree, [eq_partition])
+
+    function build_model_vec(alpha, beta; F3x4=f3x4)
+        #Need to pass through genetic code here!
+        #If you run into numerical issues with DiagonalizedCTMC, switch to GeneralCTMC instead
+        return GeneralCTMC(MolecularEvolution.MG94_F3x4(alpha, beta, GTRmat, F3x4, genetic_code=genetic_code))
+    end
+
+    function objective(alpha=1, beta=1; tree=tree, eq_freqs=eq_freqs)
+        return log_likelihood!(tree, build_model_vec(alpha, beta))
+    end
+
+    #High tolerance estimates of alpha then beta
+    alpha, beta = 1.0, 1.0
+    num_1d_optims = 3 #We'll do this many 1D optimizations, first for alpha, then beta, then alpha again and so forth
+    initial_param = 1.0 #rate must be non-negative
+    lower_bound = 0.0
+    upper_bound = 5.0
+    high_tol = 1e-4
+    for i in 1:num_1d_optims-1
+        isodd = i % 2 == 1
+        if isodd
+            alpha = brents_method_minimize(x -> -objective(x, beta), lower_bound, upper_bound, identity, high_tol)
+            @show Int(ceil(log(high_tol / (upper_bound - lower_bound)) / log(MolecularEvolution.invphi)))
+            #alpha = golden_section_maximize(x -> objective(x, beta), lower_bound, upper_bound, identity, high_tol)
+            #@show alpha1, alpha
+        else
+            beta = brents_method_minimize(x -> -objective(alpha, x), lower_bound, upper_bound, identity, high_tol)
+            #beta = golden_section_maximize(x -> objective(alpha, x), lower_bound, upper_bound, identity, high_tol)
+            #@show beta1, beta
+        end
+    end
+
+    #Low tolerance final estimate of alpha
+    """
+    the parabola thing is: 
+    take 3 values of the parameter, 
+    and eval the LL. fit the parabola, 
+    and take the max of the parabola, 
+    then swap out the worst LL in the previous set for the new max, 
+    and repeat with those 3 values 
+    // Benjamin Murrell in a teams convo
+    """
+    low_tol = 1e-7
+    ε = 1e-2
+    prev_alpha = Inf
+    maxiters = 20 #We should be quite close to the final alpha
+    iters = 0
+    xvec = [alpha - ε, alpha, alpha + ε]
+    yvec = map(x -> objective(x, beta), xvec)
+    while abs(alpha - prev_alpha) > low_tol && iters < maxiters
+        prev_alpha = alpha
+        if iters > 0 #We use this order to avoid one potential objective call
+            wipe_index = argmin(yvec)
+            max_obj = objective(alpha, beta)
+            xvec[wipe_index] = alpha
+            yvec[wipe_index] = max_obj
+            perm = sortperm(xvec)
+            xvec = xvec[perm]
+            yvec = yvec[perm]
+        end
+        alpha = MolecularEvolution.new_max(xvec, yvec)
+        iters += 1
+    end
+    #tree, alpha, beta, F3x4, eq_freqs
+    return tree, alpha, beta, f3x4, eq_freqs
+end
+
+function rescale_branchlengths!(tree, scale)
+    for node in getnodelist(tree)
+        node.branchlength *= scale
+    end
+end
 
 ##########################################
 #Possibly migrate to MolecularEvolution.jl
@@ -411,7 +510,7 @@ function MolecularEvolution.populate_tree!(
     if init_all_messages
         internal_message_init!(tree, starting_message)
     else
-        tree.parent_message = deepcopy(starting_message)
+        tree.parent_message = copy_message(starting_message)
     end
     name_dic = Dict(zip(names, 1:length(names)))
     for n in getleaflist(tree)
@@ -449,4 +548,3 @@ function MolecularEvolution.populate_tree!(
         leaf_name_transform=leaf_name_transform
     )
 end
-
