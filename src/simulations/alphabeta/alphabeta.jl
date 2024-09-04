2059de7a5912cb8f53bfc96f7d0090c69db7d449
Maximilian Danielsson <maximilian.danielsson@gmail.com>
2024-09-04 17:28:15 +0200

    Migrate some sim code

--- src/simulations/alphabeta/alphabeta.jl
+++ src/simulations/alphabeta/alphabeta.jl
@@ -8,29 +8,6 @@ end
 
 export alphabeta_setup
 
-#These were derived from a Flu dataset
-const demo_f3x4 = [ 0.293117 0.184379 0.295274 0.190878;
-                    0.342317 0.199907 0.154328 0.267101;
-                    0.231987 0.217801 0.241637 0.272234]
-
-
-const demo_nucmat = [  -0.256236 0.0697056 0.152411 0.034119;
-                        0.0697056 -0.274119 0.0596187 0.144795;
-                        0.152411 0.0596187 -0.251381 0.0393506;
-                        0.034119 0.144795 0.0393506 -0.218264]
-
-#Should maybe be in MolecularEvolution.jl
-function standard_tree_sim(ntaxa)
-    n(t) = (10*ntaxa)/(1+exp(t-10))
-    return sim_tree(ntaxa,n,ntaxa/5, mutation_rate = 0.05)
-end
-function ladder_tree_sim(ntaxa)
-    n(t) = ntaxa/10
-    return sim_tree(ntaxa,n,1.0, mutation_rate = 1/sqrt(ntaxa))
-end
-
-export standard_tree_sim, ladder_tree_sim
-
 """
     sim_alphabeta_seqs(alphavec::Vector{Float64}, betavec::Vector{Float64}, singletree, nucmat::Array{Float64,2}, f3x4::Array{Float64,2};
                             scale_total_tree_neutral_expected_subs = -1.0, outpath = "")
--- src/simulations/simulations.jl
+++ src/simulations/simulations.jl
@@ -2,6 +2,29 @@ include("alphabeta/alphabeta.jl")
 include("episodic/omegamixture.jl")
 include("episodic/omegadistribution.jl")
 
+#These were derived from a Flu dataset
+const demo_f3x4 = [ 0.293117 0.184379 0.295274 0.190878;
+                    0.342317 0.199907 0.154328 0.267101;
+                    0.231987 0.217801 0.241637 0.272234]
+
+
+const demo_nucmat = [  -0.256236 0.0697056 0.152411 0.034119;
+                        0.0697056 -0.274119 0.0596187 0.144795;
+                        0.152411 0.0596187 -0.251381 0.0393506;
+                        0.034119 0.144795 0.0393506 -0.218264]
+
+#Should maybe be in MolecularEvolution.jl
+function standard_tree_sim(ntaxa)
+    n(t) = (10*ntaxa)/(1+exp(t-10))
+    return sim_tree(ntaxa,n,ntaxa/5, mutation_rate = 0.05)
+end
+function ladder_tree_sim(ntaxa)
+    n(t) = ntaxa/10
+    return sim_tree(ntaxa,n,1.0, mutation_rate = 1/sqrt(ntaxa))
+end
+
+export standard_tree_sim, ladder_tree_sim
+
 function sim_init!(singletree, nucmat::Array{Float64}, f3x4::Array{Float64}; scale_total_tree_neutral_expected_subs = -1.0)
     eq_freqs = MolecularEvolution.F3x4_eq_freqs(f3x4)
     eq_partition = CodonPartition(1)
