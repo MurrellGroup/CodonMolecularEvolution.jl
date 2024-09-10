b60d080cf1632f18eeba4b7b21e3611c6bcd9171
Maximilian Danielsson <maximilian.danielsson@gmail.com>
2024-09-10 20:06:07 +0200

    Generalize sim_episodic_div_seqs

--- src/simulations/episodic/omegadistribution.jl
+++ src/simulations/episodic/omegadistribution.jl
@@ -1,12 +1,13 @@
-function sim_episodic_div_seqs(alphaD::Distribution, muD::Distribution, shapeD::Distribution, numsites::Int, singletree, nucmat::Array{Float64,2}, f3x4::Array{Float64,2};
-    scale_total_tree_neutral_expected_subs = -1.0, outpath = "")
+function sim_episodic_div_seqs(alphaD::Distribution, omegaD_template::T, param1D::UnivariateDistribution, param2D::UnivariateDistribution, numsites::Int, singletree, nucmat::Array{Float64,2}, f3x4::Array{Float64,2};
+    scale_total_tree_neutral_expected_subs = -1.0, outpath = "") where {T<:UnivariateDistribution}
+    @assert length(params(omegaD_template)) == 2 "The omegaD must have two degrees of freedom (two parameters), one accounting for α and the other β"
     direction = sim_init!(singletree, nucmat, f3x4, scale_total_tree_neutral_expected_subs=scale_total_tree_neutral_expected_subs)
 
     nucseq_collection = []
     sparams_collection = []
     for i in 1:numsites
-        alpha, mu, shape = rand(alphaD), rand(muD), rand(shapeD)
-        omegaD = Gamma(shape, mu / shape)
+        alpha, param1, param2 = rand(alphaD), rand(param1D), rand(param2D)
+        omegaD = T(param1, param2)
         omegas = rand(omegaD, length(getnodelist(singletree)))
         omega_dict = Dict(zip(getnodelist(singletree), omegas))
         m(n::FelNode) = [GeneralCTMC(MolecularEvolution.MG94_F3x4(alpha, alpha*omega_dict[n], nucmat, f3x4))]
@@ -17,14 +18,14 @@ function sim_episodic_div_seqs(alphaD::Distribution, muD::Distribution, shapeD::
         sample_down!(singletree, m)
         nucs_at_leaves = [n.message[1].obs for n in getleaflist(singletree)]
         push!(nucseq_collection, nucs_at_leaves)
-        push!(sparams_collection, (alpha, mu, shape, any(omega -> omega > 1.0, omegas)))
+        push!(sparams_collection, (alpha, (param1, param2), any(omega -> omega > 1.0, omegas)))
         lazyprep!(singletree, direction) #This is needed for successive sample_down! calls
     end
     nucseqs = prod(stack(nucseq_collection), dims = 2)[:]
     if outpath != ""
         write_fasta(outpath*".fasta", nucseqs, seq_names = [n.name for n in getleaflist(singletree)])
         open(outpath*".tre", "a") do io write(io, newick(singletree)) end
-        write_simparams(outpath, sparams_collection, [:α, :μ, :shape, :positive])
+        write_simparams(outpath, sparams_collection, [:α, Symbol(T), :positive])
     end
     return nucseqs, [n.name for n in getleaflist(singletree)], singletree, sparams_collection
 end
--- src/simulations/episodic/omegamixture.jl
+++ src/simulations/episodic/omegamixture.jl
@@ -1,27 +1,30 @@
-#TODO: Not limit ourselves to two omega cats
-function sim_episodic_div_seqs(alphaD::Distribution, omega1D::Distribution, omega2D::Distribution, weightD::Distribution, numsites::Int, singletree, nucmat::Array{Float64,2}, f3x4::Array{Float64,2};
+function sim_episodic_div_seqs(alphaD::Distribution, omegaDs::Vector{<:Distribution}, weightsD::Multinomial, numsites::Int, singletree, nucmat::Array{Float64,2}, f3x4::Array{Float64,2};
     scale_total_tree_neutral_expected_subs = -1.0, outpath = "")
+    @assert length(omegaDs) == length(weightsD)
+    @assert weightsD.n == length(getnodelist(singletree)) #Modeling-motivated assertion
     direction = sim_init!(singletree, nucmat, f3x4, scale_total_tree_neutral_expected_subs=scale_total_tree_neutral_expected_subs)
 
     nucseq_collection = []
     sparams_collection = []
     for i in 1:numsites
-        alpha, omegavec, weight = rand(alphaD), [rand(omega1D), rand(omega2D)], rand(weightD)
+        alpha, omegavec, weights = rand(alphaD), rand.(omegaDs), rand(weightsD)
         betavec = alpha .* omegavec
-        weights = Weights([weight, 1-weight])
+        #Fix this multinomial thingy
         models = [DiagonalizedCTMC(MolecularEvolution.MG94_F3x4(alpha, beta, nucmat, f3x4)) for beta in betavec]
-        m(n::FelNode) = [sample(models, weights)]
+        model_ind_dict = Dict(zip(getnodelist(singletree), sample([i for i=1:length(weights) for j=1:weights[i]], weightsD.n, replace=false)))
+        m(n::FelNode) = [models[model_ind_dict[n]]]
         sample_down!(singletree, m)
         nucs_at_leaves = [n.message[1].obs for n in getleaflist(singletree)]
         push!(nucseq_collection, nucs_at_leaves)
-        push!(sparams_collection, (alpha, betavec..., first(weights)))
+        push!(sparams_collection, (alpha, betavec..., weights..., any(i -> omegavec[i] > 1.0 && weights[i] > 0, 1:length(omegavec))))
         lazyprep!(singletree, direction) #This is needed for successive sample_down! calls
     end
     nucseqs = prod(stack(nucseq_collection), dims = 2)[:]
     if outpath != ""
         write_fasta(outpath*".fasta", nucseqs, seq_names = [n.name for n in getleaflist(singletree)])
         open(outpath*".tre", "a") do io write(io, newick(singletree)) end
-        write_simparams(outpath, sparams_collection, [:α, :β⁻, :β⁺, :q⁻])
+        param_symbols = vcat([:α], Symbol.("β", string.(1:length(omegaDs))), Symbol.("q", string.(1:length(weightsD))), [:positive])
+        write_simparams(outpath, sparams_collection, param_symbols)
     end
     return nucseqs, [n.name for n in getleaflist(singletree)], singletree, sparams_collection
 end
