fb0a6a93f63af8a61c8f05f0011fce5047fc1cfe
HedwigNordlinder <160422581+HedwigNordlinder@users.noreply.github.com>
2025-04-20 11:00:08 +0200

    Wierd trick borrowed from Flux.jl

--- ext/PlotsExt.jl
+++ ext/PlotsExt.jl
@@ -4,6 +4,7 @@ using CodonMolecularEvolution
 using Plots
 using Measures
 using MolecularEvolution
+using Phylo
 function gridplot(grid::CodonMolecularEvolution.FUBARgrid, results::CodonMolecularEvolution.BayesianFUBARResults; title="")
     θ = results.posterior_mean
     p = scatter(grid.alpha_ind_vec, grid.beta_ind_vec, zcolor=θ, c=:darktest, colorbar=false,
