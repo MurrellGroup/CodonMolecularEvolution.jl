20d802e5bf37da6b7943128e5e13bdcdc5cbf6b9
Tove Nordmark <tove.nordmark.01@gmail.com>
2025-06-27 18:58:14 +0200

    Trying to get plots to work

--- src/difFUBAR/skbDifFUBAR.jl
+++ src/difFUBAR/skbDifFUBAR.jl
@@ -1,4 +1,4 @@
-using  Distributions, MCMCChains, LinearAlgebra, Phylo, FASTX, MolecularEvolution, CodonMolecularEvolution, Phylo, EllipticalSliceSampling, AbstractMCMC, NNlib
+using  Statistics, Distributions, MCMCChains, LinearAlgebra, Phylo, FASTX, MolecularEvolution, CodonMolecularEvolution, Plots, EllipticalSliceSampling, AbstractMCMC, NNlib, StatsBase
 
 #= This struct is a bit f****d
     because it is only applicable to difFUBAR
@@ -79,13 +79,37 @@ function construct_grid_index_matrix(grid::SuppressionGrid)
     return index_matrix
 end
 
+function calculate_alloc_grid_and_theta(grid::SuppressionGrid, ambient_samples::Vector{Vector{Float64}}, burnin::Int=0)
+    """
+    Samples the allocation grid from the ambient samples
+    """
+    n_samples = size(ambient_samples, 1)
+    n_categories = size(grid.con_lik_matrix, 1)
+    n_sites = size(grid.con_lik_matrix, 2)
+    alloc_grid = zeros(Int64, size(grid.con_lik_matrix))
+    theta = zeros(Float64, n_categories)
+    for i = burnin+1:size(ambient_samples, 1)
+        probability_vector = probability_vector_given_parameters(grid, ambient_samples[i])
+        theta .+= probability_vector # Accumulate the probability vector
+        v = zeros(Float64, n_categories)
+        for i = 1:n_sites
+            for j = 1:n_categories
+                v[j] = probability_vector[j] * grid.con_lik_matrix[j, i]
+            end
+            samp = sample(1:n_categories, Weights(v))
+            alloc_grid[samp, i] += 1 # Increment the allocation for the sampled category
+        end
+    end
+    return alloc_grid, theta ./ (n_samples - burnin)
+end
+
 function skbdifFUBAR(seqnames, seqs, treestring, tags, outpath,
     tag_colors; pos_thresh=0.95, iters=2500,
     burnin::Int=div(iters, 5), concentration=0.1, binarize=false, verbosity=1,
     exports=true, exports2json=false, code=MolecularEvolution.universal_code,
     optimize_branch_lengths=false, version=nothing, t=0)
     # TODO: Split this up into multiple fcts because the setup is mostly shared with difFUBAR
-
+    total_time = @elapsed begin
     analysis_name = outpath
     leaf_name_transform = CodonMolecularEvolution.generate_tag_stripper(tags)
     plot_collection = NamedTuple[]
@@ -112,15 +136,15 @@ function skbdifFUBAR(seqnames, seqs, treestring, tags, outpath,
             F3x4_freqs,
             code,
             verbosity=verbosity,
-            foreground_grid=2,
-            background_grid=2,
+            foreground_grid=6,
+            background_grid=4,
             version=version,
             t=t)
 
     transition_functions = [s -> CodonMolecularEvolution.quintic_smooth_transition(s, 0, 1) for i = 1:4]
     # Define the masks for the suppression parameters
     masks = ones(Bool, (4, size(con_lik_matrix)[1]))
-    print(size(masks))
+    
     codon_param_mat = reduce(hcat, codon_param_vec)'#reshape(codon_param_vec, (size(codon_param_vec, 1), 3)) # Reshape to codon, param
     codon_param_index_mat = reduce(hcat, codon_param_index_vec)' # Reshape to codon, param index
 
@@ -128,9 +152,6 @@ function skbdifFUBAR(seqnames, seqs, treestring, tags, outpath,
     masks[2, :] .= codon_param_mat[:,3] .> 1 # ω_2 > 1
     masks[3, :] .= codon_param_mat[:,2] .> codon_param_mat[:,3] # ω_1 > ω_2
     masks[4, :] .= codon_param_mat[:,3] .> codon_param_mat[:,2] # ω_1 < ω_2
-    println("Masks: ", masks)
-    println(size(masks))
-    println(masks[1, :])
     grid = SuppressionGrid(con_lik_matrix, log_con_lik_matrix, codon_param_mat, codon_param_index_mat, alphagrid, omegagrid, background_omega_grid, param_kinds, ["ω_1>ω_2", "ω_1<ω_2", "ω_1>1", "ω_2>1"], transition_functions, masks)
     squared_distance_function = (i, j) -> sum((grid.codon_param_index_mat[i, :].- grid.codon_param_index_mat[j, :]).^2) # Squared distance function for the kernel
     c = 1.0
@@ -139,18 +160,23 @@ function skbdifFUBAR(seqnames, seqs, treestring, tags, outpath,
     sigma_s = 0.1 # Standard deviation for the normal prior for suppression parameters
     ess_model = define_ess_model(grid, sigma_s, sigma_0)
     # Run the ESS sampling
+    sample_time = @elapsed begin
     ambient_samples = AbstractMCMC.sample(ess_model, ESS(), iters,
         progress=true)
-
-    # TODO: Sample from the posterior
-    # TODO: summary statistics, visualizations, save results
+    alloc_grid, theta = calculate_alloc_grid_and_theta(grid, ambient_samples, burnin)
+    end # End of sample_time
+    df, plots_named_tuple = CodonMolecularEvolution.difFUBAR_tabulate_and_plot(analysis_name, pos_thresh, alloc_grid, codon_param_vec, alphagrid, omegagrid, tag_colors, verbosity=verbosity, exports=exports)
+    end # End of total_time
+    json = CodonMolecularEvolution.dNdS2JSON(CodonMolecularEvolution.difFUBAR2JSON(), (outpath=analysis_name, df=df, θ=theta, posterior_mat=alloc_grid ./ sum(alloc_grid, dims=1), categories=reduce(hcat, codon_param_vec)', tags=tags, tree=shallow_tree, LL=LL, timers = (total_time, fit_time, grid_time, sample_time), treestring=treestring, seqnames=seqnames, seqs=seqs, leaf_name_transform=leaf_name_transform, pos_thresh=pos_thresh, iters=iters, burnin=burnin, concentration=concentration, binarize=binarize, exports=exports2json))
+    push!(plot_collection, plots_named_tuple)
+    return df, (alloc_grid, ambient_samples, grid, tag_colors), merge(plot_collection...)
 end
 
 
 analysis_name = "output/Ace2"
 seqnames,seqs = read_fasta("test/data/Ace2_tiny/Ace2_tiny_tagged.fasta");
 treestring, tags, tag_colors = import_colored_figtree_nexus_as_tagged_tree("test/data/Ace2_tiny/Ace2_tiny_tagged.nex")
-skbdifFUBAR(seqnames, seqs, treestring, tags, analysis_name, tag_colors)
+df, results = skbdifFUBAR(seqnames, seqs, treestring, tags, analysis_name, tag_colors)
 
 
 
